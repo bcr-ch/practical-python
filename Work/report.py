8cd089a0046887837bc8656d032bf99289fdec72
bcr <bcr@devkode.lan>
2020-07-28 13:39:58 +0200

    Updated

--- Work/report.py
+++ Work/report.py
@@ -20,7 +20,19 @@ def read_portfolio(filename):
            portfolio.append(d)
     return portfolio
 
-if len(sys.argv) == 2:
+def read_prices(filename)
+    prices = []
+
+    with open(pricefile, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            holdings = (row[0], float(row[1]))
+            prices.append(holdings)
+        return prices
+
+if len(sys.argv) == 3:
     filename = sys.argv[1]
+    pricefile = sys.argv[2]
 else:
     filename = 'Data/portfolio.csv'
+    pricefile = 'Data/prices.csv'
